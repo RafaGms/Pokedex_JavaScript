ec42f990d620c8a3286b101ab63a1488983037bc
RafaGms <faelgomes2003@gmail.com>
2023-03-09 21:27:03 -0300

    Pokedex-Beta

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+const offset = 0;
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+const listPokemon = document.getElementById('listPokemon')
+
+fetch(url)
+.then((responsive) => responsive.json())
+.then((bodyJason) => bodyJason.results)
+.then((pokemonList)  => {
+   for (let i = 0; i < pokemonList.length; i++) {
+      const pokemon = pokemonList[i];
+      listPokemon.innerHTML += pokemonConvertToHtml(pokemon);
+   }
+})
+.catch((error) => console.error(error))
+
+
+function pokemonConvertToHtml(pokemon){
+   return `<li class="pokemon">
+   <span class="number">#001</span>
+   <span class="name">${pokemon.name}</span>
+
+   <div class="details">
+      <ol class="types">
+         <li class="type">glass</li>
+         <li class="type">poison</li>
+      </ol>
+      <img
+         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         alt="${pokemon.name}">
+   </div>
+</li>`
+}
+
+
